dda014f4469a18b5ef8a0f5c3a3dc57b4d79e621
HamidRezaAshkiyan <HamidRezaAshkiyan@gmail.com>
2021-06-28 13:59:20 +0330

    Add project files.

---
+++ Main.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace USB_and_Partition_List
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.USB = new System.Windows.Forms.TabPage();
+            this.btnUSBSubmit = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnPartitionSumbit = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.USB.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.USB);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tabControl1.Font = new System.Drawing.Font("Vazir", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.tabControl1.Location = new System.Drawing.Point(0, 119);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.tabControl1.RightToLeftLayout = true;
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(492, 150);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // USB
+            // 
+            this.USB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(156)))), ((int)(((byte)(18)))));
+            this.USB.Controls.Add(this.btnUSBSubmit);
+            this.USB.Controls.Add(this.comboBox1);
+            this.USB.Location = new System.Drawing.Point(4, 27);
+            this.USB.Name = "USB";
+            this.USB.Padding = new System.Windows.Forms.Padding(3);
+            this.USB.Size = new System.Drawing.Size(484, 119);
+            this.USB.TabIndex = 0;
+            this.USB.Text = "فلش درایور";
+            // 
+            // btnUSBSubmit
+            // 
+            this.btnUSBSubmit.Location = new System.Drawing.Point(70, 40);
+            this.btnUSBSubmit.Name = "btnUSBSubmit";
+            this.btnUSBSubmit.Size = new System.Drawing.Size(75, 26);
+            this.btnUSBSubmit.TabIndex = 1;
+            this.btnUSBSubmit.Text = "ثبت";
+            this.btnUSBSubmit.UseVisualStyleBackColor = true;
+            this.btnUSBSubmit.Click += new System.EventHandler(this.btnUSBSubmit_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(200, 40);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.comboBox1.Size = new System.Drawing.Size(250, 26);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(196)))), ((int)(((byte)(15)))));
+            this.tabPage2.Controls.Add(this.btnPartitionSumbit);
+            this.tabPage2.Controls.Add(this.comboBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 27);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(484, 119);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "هارد";
+            // 
+            // btnPartitionSumbit
+            // 
+            this.btnPartitionSumbit.Location = new System.Drawing.Point(70, 40);
+            this.btnPartitionSumbit.Name = "btnPartitionSumbit";
+            this.btnPartitionSumbit.Size = new System.Drawing.Size(75, 26);
+            this.btnPartitionSumbit.TabIndex = 2;
+            this.btnPartitionSumbit.Text = "ثبت";
+            this.btnPartitionSumbit.UseVisualStyleBackColor = true;
+            this.btnPartitionSumbit.Click += new System.EventHandler(this.btnPartitionSumbit_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(200, 40);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.comboBox2.Size = new System.Drawing.Size(250, 26);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Vazir", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(79, 31);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(325, 53);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "با این برنامه میتوانید فایل های مخفی شده یا خراب شده توسط ویروس بر روی فلش یا هار" +
+    "د خود را بازیابی کنید.\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(169)))), ((int)(((byte)(244)))));
+            this.ClientSize = new System.Drawing.Size(492, 269);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "پاک کننده ویروس";
+            this.tabControl1.ResumeLayout(false);
+            this.USB.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage USB;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btnUSBSubmit;
+        private System.Windows.Forms.Button btnPartitionSumbit;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace USB_and_Partition_List
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            var driveList = DriveInfo.GetDrives();
+
+            foreach (var drive in driveList)
+            {
+                if (drive.DriveType == DriveType.Removable)
+                    comboBox1.Items.Add(drive);
+
+                if (drive.DriveType == DriveType.Fixed)
+                    comboBox2.Items.Add(drive);
+            }
+        }
+
+        private void btnUSBSubmit_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("لطفا یکی از این گزینه ها را انتخاب کنید");
+            }
+            else
+            {
+                var driveName = comboBox1.SelectedItem.ToString();
+
+                var process   = new System.Diagnostics.Process();
+                var startInfo = new System.Diagnostics.ProcessStartInfo();
+                startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                startInfo.FileName    = "cmd.exe";
+                startInfo.Arguments   = $@"/C attrib -s -h /s /d {driveName}*.*";
+                process.StartInfo     = startInfo;
+                process.Start();
+            }
+        }
+
+        private void btnPartitionSumbit_Click(object sender, EventArgs e)
+        {
+            var driveName = comboBox2.SelectedItem.ToString();
+
+            var process   = new System.Diagnostics.Process();
+            var startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName    = "cmd.exe";
+            startInfo.Arguments   = $@"/C attrib -s -h /s /d {driveName}*.*";
+            process.StartInfo     = startInfo;
+            process.Start();
+        }
+    }
+}
